7e5774528106be43f800ee13150643b89f99f373
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-11-21 10:38:20 -0500

    Added user level to Employee class.

--- project/Employee.java
+++ project/Employee.java
@@ -8,17 +8,24 @@ public class Employee
     private int id;
     private String name;
     private String title;
+    private UserLevel level;
 
     //  ------------------------------------------------------------------------
-    public Employee(int id, String name, String title) {
+    public Employee(int id, String name, String title, UserLevel level) {
         this.id = id;
         this.name = name;
         this.title = title;
+        this.level = level;
     }
 
     //  ------------------------------------------------------------------------
     public int getId() {
         return id;
+    }
+
+    //  ------------------------------------------------------------------------
+    public UserLevel getLevel() {
+        return level;
     }
 
     //  ------------------------------------------------------------------------
