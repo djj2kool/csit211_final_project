a652372ca9917712fadaddce98619245215a63a9
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-11-30 13:07:52 -0500

    Added DatabaseListener interface.

---
+++ project/DatabaseListener.java
@@ -0,0 +1,9 @@
+//  Darren Jackson, Chintan Rami, Louis Slavotinek, Raymond Zegles
+//  DatabaseListener.java
+//  Listener for database events.
+//  Visual Studio Code
+
+public interface DatabaseListener
+{
+    void onRecordAdded();
+}
