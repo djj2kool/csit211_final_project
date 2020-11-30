2c990f71b0c853ac96914c4fd18b08b78c09ceb4
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-11-30 14:11:22 -0500

    Added Javadoc comments.

--- project/RentalStatus.java
+++ project/RentalStatus.java
@@ -5,20 +5,36 @@
 
 public enum RentalStatus
 {
+    /**
+     * Open (rental in progress)
+     */
     OPEN("Open"),
+    /**
+     * Closed (rental completed)
+     */
     CLOSED("Closed");
 
     final static int OPEN_INT   = 0;
     final static int CLOSED_INT = 1;
 
+    /**
+     * Display string describing this enum.
+     */
     private final String string;
 
-    //  ------------------------------------------------------------------------
+    /**
+     * Constructs a RentalStatus enum.
+     * @param string String for toString method to return
+     */
     private RentalStatus(String string) {
         this.string = string;
     }
 
-    //  ------------------------------------------------------------------------
+    /**
+     * Converts an int to a RentalStatus enum.
+     * @param value int value to convert from
+     * @return RentalStatus matching specified int
+     */
     static public RentalStatus fromInt(int value) {
         switch (value) {
             default:
@@ -29,7 +45,10 @@ public enum RentalStatus
         }
     }
 
-    //  ------------------------------------------------------------------------
+    /**
+     * Converts this RentalStatus enum to an int.
+     * @return int value matching this enum
+     */
     public int toInt() {
         switch (this) {
             default:
@@ -39,7 +58,10 @@ public enum RentalStatus
                 return CLOSED_INT;        }
     }
 
-    //  ------------------------------------------------------------------------
+    /**
+     * Returns a string representation of this enum.
+     * @return a string representation of the filter
+     */
     @Override
     public String toString() {
         return this.string;
