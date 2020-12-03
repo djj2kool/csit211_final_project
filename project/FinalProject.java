06349e1d8bc930a3276807235554951dcc6b9cea
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-12-03 12:56:22 -0500

    Fixed issue with modeless rental dialog not closing after main window is closed.

--- project/FinalProject.java
+++ project/FinalProject.java
@@ -11,15 +11,25 @@ import javafx.stage.Stage;
 
 public class FinalProject extends Application
 {
+    /**
+     * Application primary stage.
+     * Used as owner of child dialog boxes so application exits correctly
+     * if there are modeless dialogs active when closing the main window.
+     */
+    public static Stage primaryStage;
+
     //  ------------------------------------------------------------------------
     public static void main(String[] args) {
         launch(args);
     }
 
     //  ------------------------------------------------------------------------
+    @Override
     public void start(Stage primaryStage) throws Exception {
         Pane pane   = null;
         Scene scene = null;
+
+        FinalProject.primaryStage = primaryStage;
 
         primaryStage.setTitle("CDLR Corporation - Vehicle Rentals");
         pane = (Pane)FXMLLoader.load(getClass().getResource("Main.fxml"));
