8c277d45d5ffc2faac4da068434822b02c92b17d
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-12-04 18:24:41 -0500

    Add button in add new employee dialog is now disabled until all fields are valid.

--- src/finalProject/AddEmployeeDialogController.java
+++ src/finalProject/AddEmployeeDialogController.java
@@ -13,6 +13,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
@@ -22,19 +23,33 @@ public class AddEmployeeDialogController extends DialogController<Employee> impl
     @FXML private TextField titleField;
     @FXML private ComboBox<UserLevel> levelComboBox;
 
+    @FXML private Button addButton;
+
     Employee employee = null;
 
     //  ------------------------------------------------------------------------
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //  Populate user levels combobox
         ObservableList<UserLevel> levels =
-        FXCollections.observableArrayList(
-            UserLevel.ASSOCIATE,
-            UserLevel.EXECUTIVE,
-            UserLevel.ADMIN
-        );
+            FXCollections.observableArrayList(
+                UserLevel.ASSOCIATE,
+                UserLevel.EXECUTIVE,
+                UserLevel.ADMIN
+            );
         levelComboBox.setItems(levels);
         levelComboBox.setValue(levels.get(0));
+
+        //  Add button is disabled while fields are empty
+        addButton.setDisable(true);
+
+        nameField.textProperty().addListener((observable, oldValue, newValue) -> {
+            onFieldsChanged();
+        });
+
+        titleField.textProperty().addListener((observable, oldValue, newValue) -> {
+            onFieldsChanged();
+        });
     }
 
     //  ------------------------------------------------------------------------
@@ -56,5 +71,14 @@ public class AddEmployeeDialogController extends DialogController<Employee> impl
     @FXML
     private void onCancelButton(ActionEvent event) {
         stage.close();
+    }
+
+    //  ------------------------------------------------------------------------
+    private void onFieldsChanged() {
+        //  Add button is disabled while fields are empty
+        addButton.setDisable(
+            nameField.getText().isBlank() ||
+            titleField.getText().isBlank()
+        );
     }
 }
