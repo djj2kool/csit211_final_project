a8dec740d8ae7ebf1d59002f061f3fcac07ffe58
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-12-04 18:49:05 -0500

    Caluclated price fields in rental dialogs are not longer editable.

--- src/finalProject/RentalDialogController.java
+++ src/finalProject/RentalDialogController.java
@@ -13,7 +13,6 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
 
 public abstract class RentalDialogController extends DialogController<Rental>
