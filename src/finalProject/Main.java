fa9b22c140af530d1d3b1a05a8d3e5f8bd54422d
Tony Slavotinek <apokryphos@users.noreply.github.com>
2020-12-04 17:45:38 -0500

    Added Javadoc comments.

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -1,9 +1,20 @@
+//  Darren Jackson, Chintan Rami, Louis Slavotinek, Raymond Zegles
+//  Main.java
+//  Application entry point.
+//  Eclipse, Visual Studio Code
+
 package finalProject;
 
 import javafx.application.Application;
 
 public class Main {
+	/**
+	 * Application entry point.
+	 * @param args
+	 */
 	public static void main(String[] args) {
+		//	Launching this way removes the need to use --module-path and
+		//	--add-module with JavaFX when running from an IDE.
 		Application.launch(FinalProject.class);
 	}
 }
